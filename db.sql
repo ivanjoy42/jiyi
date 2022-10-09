c6b522bf79af7f828898e114049a0de4d79a5507
ivanjoy42 <ivanjoy42@163.com>
2022-10-09 23:01:08 +0800

    更新kind相关表结构

--- db.sql
+++ db.sql
@@ -1,13 +1,13 @@
 DROP TABLE IF EXISTS `card`;
 CREATE TABLE IF NOT EXISTS `card` (
   `card_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `kind` int(10) unsigned NOT NULL,
+  `kind_id` int(10) unsigned NOT NULL,
   `front` varchar(100) NOT NULL,
   `back` varchar(100) NOT NULL,
   `helper` varchar(100) NOT NULL,
   `pinyin` varchar(100) NOT NULL,
   PRIMARY KEY (`card_id`),
-  UNIQUE KEY `kind_front` (`kind`,`front`)
+  UNIQUE KEY `kind_id_front` (`kind_id`,`front`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS `card_deck`;
@@ -22,9 +22,10 @@ CREATE TABLE IF NOT EXISTS `card_deck` (
 DROP TABLE IF EXISTS `deck`;
 CREATE TABLE IF NOT EXISTS `deck` (
   `deck_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `kind` int(10) unsigned NOT NULL,
+  `kind_id` int(10) unsigned NOT NULL,
   `deck_name` varchar(100) NOT NULL,
-  PRIMARY KEY (`deck_id`)
+  PRIMARY KEY (`deck_id`),
+  UNIQUE KEY `kind_id_deck_name` (`kind_id`,`deck_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS `learn`;
